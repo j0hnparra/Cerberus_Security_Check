991ed71951cde897200b418ff33f5c3a3adf4759
John Parra <jparra13@outlook.com>
2019-12-21 12:39:17 -0700

    Initial Commit

---
+++ app/src/main/java/com/cerberus/securitycheck/ui/feed/FeedFragment.kt
@@ -0,0 +1,32 @@
+package com.cerberus.securitycheck.ui.feed
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.cerberus.securitycheck.R
+import com.cerberus.securitycheck.ui.notifications.FeedViewModel
+
+class FeedFragment : Fragment() {
+
+    private lateinit var feedViewModel: FeedViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        feedViewModel =
+            ViewModelProviders.of(this).get(FeedViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        feedViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
