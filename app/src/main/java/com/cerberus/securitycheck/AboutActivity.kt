652671bd528f8f2682bbf417d4f75459660e9c00
muuuco <45134362+muuuco@users.noreply.github.com>
2019-12-27 17:01:02 -0700

    Added top bar nav menu
    Added values in resources for top bar nav menu text
    
    Added AboutActivity.kt for About page
    
    Adjusted AndroidManifest.xml to account for About page activity

---
+++ app/src/main/java/com/cerberus/securitycheck/AboutActivity.kt
@@ -0,0 +1,19 @@
+package com.cerberus.securitycheck
+
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_about.*
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+        setSupportActionBar(toolbar)
+    }
+
+
+}
--- app/src/main/java/com/cerberus/securitycheck/MainActivity.kt
+++ app/src/main/java/com/cerberus/securitycheck/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.cerberus.securitycheck
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -25,5 +27,26 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_about -> true
+            R.id.action_faq -> true
+            R.id.action_feedback -> true
+            R.id.action_privacy -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
