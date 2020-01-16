c91e902c7cee9c1685f60c2b534f53e453c76aab
muuuco <45134362+muuuco@users.noreply.github.com>
2020-01-16 14:58:45 -0700

    Added text box and search button for future search function Removed test file

--- app/src/main/java/com/cerberus/securitycheck/AboutActivity.kt
+++ app/src/main/java/com/cerberus/securitycheck/AboutActivity.kt
@@ -1,19 +1,13 @@
 package com.cerberus.securitycheck
 
-import android.os.Bundle
-import android.widget.TextView
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-
-import kotlinx.android.synthetic.main.activity_about.*
+import android.os.Bundle
 
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
-        setSupportActionBar(toolbar)
+        setContentView(R.layout.activity_about
+        )
     }
-
-
 }
--- app/src/main/java/com/cerberus/securitycheck/TestTyler.kt
+++
@@ -1,2 +0,0 @@
-package com.cerberus.securitycheck
-
