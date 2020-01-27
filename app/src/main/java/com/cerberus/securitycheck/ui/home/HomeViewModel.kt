73eb26590474785509f208800ca3bd4cd9f1ab04
muuuco <45134362+muuuco@users.noreply.github.com>
2020-01-27 12:08:15 -0700

    Placeholder text added to Home screen. New line added to separate Breach results.

--- app/src/main/java/com/cerberus/securitycheck/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/cerberus/securitycheck/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "About us and the application...."
+        value = "All Breaches via HaveIBeenPwned"
     }
     val text: LiveData<String> = _text
 }
